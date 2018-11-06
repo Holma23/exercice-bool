a95503acef22497511d53c6adcd857d0830180bf
Holma23 <44214084+Holma23@users.noreply.github.com>
2018-11-06 23:46:47 +0100

    Add files via upload

---
+++  exercice-bool/bool.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+
+
+<table border="1">
+
+<th> Variable </th>
+    <th>  Valeur booléenne</th>
+    <tr>
+        <td> $a="0"</td>//$a chaine vide
+        <td> false</td>
+    </tr>
+    <tr>
+        <td> $b="TRUE"</td>
+        <td> true</td>
+    </tr>
+    <tr>
+        <td> $c=FALSE</td>
+        <td> false</td>
+    </tr>
+    <tr>
+        <td> $d=($a OR $b)</td>
+        <td> true</td>
+    </tr>
+    <tr>
+        <td> $e=($a AND $c)</td>
+        <td> false</td>
+    </tr>
+    <tr>
+        <td> $f=($a XOR $b)</td>
+        <td> true</td>
+    </tr>
+
+
+
+</table>
